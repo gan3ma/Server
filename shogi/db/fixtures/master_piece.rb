52a39b60930aaba5a809f08c10109a87217a0fd6
yuemori <yuemori@aiming-inc.com>
2015-08-01 16:16:44 +0900

    Fix master_piece position

--- shogi/db/fixtures/master_piece.rb
+++ shogi/db/fixtures/master_piece.rb
@@ -122,120 +122,119 @@ MasterPiece.seed do |s|
   s.id = 21
   s.name = 'oh'
   s.posx = 5
-  s.posy = 9
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 22
   s.name = 'kin'
-  s.posx = 6
-  s.posy = 9
+  s.posx = 4
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 23
   s.name = 'kin'
-  s.posx = 4
-  s.posy = 9
+  s.posx = 6
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 24
   s.name = 'gin'
-  s.posx = 7
-  s.posy = 9
+  s.posx = 3
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 25
   s.name = 'gin'
-  s.posx = 3
-  s.posy = 9
+  s.posx = 7
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 26
   s.name = 'keima'
-  s.posx = 8
-  s.posy = 9
+  s.posx = 2
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 27
   s.name = 'keima'
-  s.posx = 2
-  s.posy = 9
+  s.posx = 8
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 28
   s.name = 'kyosya'
-  s.posx = 9
-  s.posy = 9
+  s.posx = 1
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 29
   s.name = 'kyosya'
-  s.posx = 1
-  s.posy = 9
+  s.posx = 9
+  s.posy = 1
 end
 MasterPiece.seed do |s|
   s.id = 30
   s.name = 'kaku'
   s.posx = 8
-  s.posy = 8
+  s.posy = 2
 end
 MasterPiece.seed do |s|
   s.id = 31
   s.name = 'hisya'
   s.posx = 2
-  s.posy = 8
+  s.posy = 2
 end
 MasterPiece.seed do |s|
   s.id = 32
   s.name = 'fu'
-  s.posx = 9
-  s.posy = 7
+  s.posx = 1
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 33
   s.name = 'fu'
-  s.posx = 8
-  s.posy = 7
+  s.posx = 2
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 34
   s.name = 'fu'
-  s.posx = 7
-  s.posy = 7
+  s.posx = 3
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 35
   s.name = 'fu'
-  s.posx = 6
-  s.posy = 7
+  s.posx = 4
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 36
   s.name = 'fu'
   s.posx = 5
-  s.posy = 7
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 37
   s.name = 'fu'
-  s.posx = 4
-  s.posy = 7
+  s.posx = 6
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 38
   s.name = 'fu'
-  s.posx = 3
-  s.posy = 7
+  s.posx = 7
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 39
   s.name = 'fu'
-  s.posx = 2
-  s.posy = 7
+  s.posx = 8
+  s.posy = 3
 end
 MasterPiece.seed do |s|
   s.id = 40
   s.name = 'fu'
-  s.posx = 1
-  s.posy = 7
+  s.posx = 9
+  s.posy = 3
 end
-
